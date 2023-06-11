3ae4fd0fb83782779d71648ce732c17f9fd74411
ROMAN BEZNOSIUK <89782854+ROMAN1411@users.noreply.github.com>
2023-06-11 22:00:09 +0300

    Part-01

---
+++ js/test.js
@@ -0,0 +1,20 @@
+console.log('-------------------------------');
+{
+    function deliverPizza(pizzaName) {
+        return `Delivering ${pizzaName} pizza.`;
+    }
+
+    function makePizza(pizzaName) {
+        return `Pizza ${pizzaName} is being prepared, please wait...`;
+    }
+
+    // Chande code below this line
+    function makeMessage(pizzaName, callback) {
+        // console.log(callback);
+        return callback(pizzaName);
+    }
+
+    console.log(makeMessage('Royal Grand', makePizza));
+    console.log(makeMessage('Ultracheese', deliverPizza));
+}
+console.log('-------------------------------');
