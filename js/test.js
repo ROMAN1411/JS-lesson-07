cdd53c866aa208d22e15517b1bb1ee482c096072
ROMAN BEZNOSIUK <89782854+ROMAN1411@users.noreply.github.com>
2023-06-12 21:47:57 +0300

    Test

--- js/test.js
+++ js/test.js
@@ -18,3 +18,38 @@ console.log('-------------------------------');
     console.log(makeMessage('Ultracheese', deliverPizza));
 }
 console.log('-------------------------------');
+{
+    const pizzaPalace = {
+        pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+        order(pizzaName, onSuccess, onError) {
+            for (const pizza of this.pizzas) {
+                if (pizza === pizzaName) {
+                    return makePizza(pizzaName);
+                }
+            }
+            return onOrderError(pizzaName);
+        },
+    };
+    // Change code above this line
+
+    // Callback for onSuccess
+    function makePizza(pizzaName) {
+        return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+    }
+
+    // Callback for onError
+    function onOrderError(error) {
+        return `Error! ${error}`;
+    }
+
+    // Method calls with callbacks
+    console.log(pizzaPalace.order('Smoked', makePizza, onOrderError));
+    // pizzaPalace.order('Smoked', makePizza, onOrderError);
+    console.log(pizzaPalace.order('Four meats', makePizza, onOrderError));
+    // pizzaPalace.order('Four meats', makePizza, onOrderError);
+    console.log(pizzaPalace.order('Big Mike', makePizza, onOrderError));
+    // pizzaPalace.order('Big Mike', makePizza, onOrderError);
+    console.log(pizzaPalace.order('Vienna', makePizza, onOrderError));
+    // pizzaPalace.order('Vienna', makePizza, onOrderError);
+}
+console.log('-------------------------------');
